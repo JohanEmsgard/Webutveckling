767e96ca3bf3a96fe800d90127980dbb38883021
JohanEmsgard <emsgard.johan@elev.ga.ntig.se>
2022-05-10 13:26:19 +0200

    jhdas

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-document.querySelector("#supersonic").addEventListener("change", changesupersonic);
+document.querySelector("#supersonic").addEventListener("change", changesupersonic2);
 
-function changesupersonic(event) {
+function changesupersonic2(event) {
     if(event.target.value === "Chaosemeralds") {
-        let changesupersonic = document.querySelector(".transform");
-        changesupersonic.setAttribute("src", "./SFSBSuperSonic.webp");
+        let changesupersonic2 = document.querySelector(".supersonic2");
+        changesupersonic2.setAttribute("src", "./SFSBSuperSonic.webp");
 
     }
 }
+
